465233fd67cf0b875794da6a7303bf10b42e4d87
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-05-03 01:50:50 +0200

    initialisation

---
+++ src/Components/LOAD/load.js
@@ -0,0 +1,7 @@
+const load = () => {
+    return(
+        <h1>I'm the loader</h1>
+    )
+}
+
+export default load;
---
+++ src/Screens/screenLoad.js
@@ -0,0 +1,9 @@
+import Load from "../Components/LOAD/load"
+
+const screenLoad = ()=>{
+    return(
+        <Load/>
+    )
+}
+
+export default screenLoad;
